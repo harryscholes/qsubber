849cfa63c59753f122e829f35b3477d99a6440d5
harryscholes <harryscholes@gmail.com>
2018-06-13 12:36:04 +0100

    Save $outputdir to root of tar file

--- epilogue.sh
+++ epilogue.sh
@@ -2,7 +2,8 @@
 if [ ${CODE_DONE:-x} = true ]; then
     echo CODE_DONE $($the_time)
     unset LD_LIBRARY_PATH  # Prevents segmentation faults
-    tar zcvf $results_dir/${the_date}_${JOB_NAME}_${JOB_ID}${sti}.tar.gz $output_dir
+    cd $output_dir \
+    && tar zcvf $results_dir/${the_date}_${JOB_NAME}_${JOB_ID}${sti}.tar.gz *
     echo DONE $($the_time)
 else
     echo ERROR $($the_time)
