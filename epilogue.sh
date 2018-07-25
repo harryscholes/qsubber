f3f87579c44d367c650d5fd33e0117e407854582
harryscholes <harryscholes@gmail.com>
2018-07-25 08:44:16 +0200

    Fix timestamp

--- epilogue.sh
+++ epilogue.sh
@@ -1,6 +1,7 @@
 # If code ran successfully, save the contents of Scratch
 if [ ${CODE_DONE:-x} = true ]; then
-    echo CODE_DONE $(date +%H:%M:%S)
+    echo CODE_DONE
+    date +%H:%M:%S
     unset LD_LIBRARY_PATH  # Prevents segmentation faults
     cd $output_dir \
     && tar zcvf $results_dir/${the_date}_${JOB_NAME}_${JOB_ID}${sti}.tar.gz *
